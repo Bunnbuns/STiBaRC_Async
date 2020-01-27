9d6bbfd99793b581b940616ee03978b18a1ad603
Ben Border <ben@bunnbuns.net>
2020-01-27 13:36:58 -0500

    links fixed

--- js/global.js
+++ js/global.js
@@ -228,8 +228,12 @@ function toLink(id, item){
         if (item["deleted"]){
             item["title"] = "Post deleted";
         }
-        posts = posts.concat('<div class="post"><a style="font-size:100%;text-decoration:none;" href="post.html?id=').concat(id).concat('"><b>').concat(item['title'].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")).concat('</b></a><br/>Posted by: <a href="user.html?id=').concat(item['poster']).concat('">').concat(item['poster']).concat("</a><br/>&#8679; "+item['upvotes']+" &#8681; "+item['downvotes']+"</div>");
-        
+        var title = item['title'].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        //posts = posts.concat('<a class="post" " href="post.html?id=').concat(id).concat('"><b>').concat().concat('</b><div class="1"></div>&#8679; "+item['upvotes']+" &#8681; "+item['downvotes']'+"</a>"+'<div class="2">Posted by: <a href="user.html?id=').concat(item['poster']).concat('">').concat(item['poster']).concat("</a></div>");
+        //
+        //document.getElementById("list").innerHTML = document.getElementById("list").innerHTML.concat('<div class="post"><a style="font-size:100%;text-decoration:none;" href="post.html?id=').concat(id).concat('"><b>').concat(item['title'].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")).concat('</b></a><br/>Posted by: <a href="user.html?id=').concat(item['poster']).concat('">').concat(item['poster']).concat("</a><br/>&#8679; "+item['upvotes']+" &#8681; "+item['downvotes']+"</div><br/>");
+        //
+        posts += '<div class="post"><a class="overlay" style="font-size:100%;text-decoration:none;" href="post.html?id='+id+'"><b>'+title+'</b></a><div class="inner"><br>Posted by: <a href="user.html?id='+item['poster']+'">'+item['poster']+'</a><br>&#8679; '+item['upvotes']+' &#8681; '+item['downvotes']+'</a></div></div>';
         lastid = id;
       
     }catch (err){
@@ -251,6 +255,14 @@ function loadPosts(){
     };
     xhttp.open("GET", "https://api.stibarc.com/v2/getposts.sjs", true);
     xhttp.send();
+}
+// overlay //
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
 }
 
 // get url path from hash //

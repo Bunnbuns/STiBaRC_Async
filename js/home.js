883a08f0b445427dbf63e0bcd34100fea4758933
Ben Border <ben@bunnbuns.net>
2020-11-21 15:56:18 -0500

    load more button

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-var posts = '';
+var postsHTML = '';
 function toLink(id, item){
     try{
         if (item["deleted"]){
@@ -9,7 +9,7 @@ function toLink(id, item){
         //
         //document.getElementById("list").innerHTML = document.getElementById("list").innerHTML.concat('<div class="post"><a style="font-size:100%;text-decoration:none;" href="post.html?id=').concat(id).concat('"><b>').concat(item['title'].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")).concat('</b></a><br/>Posted by: <a href="user.html?id=').concat(item['poster']).concat('">').concat(item['poster']).concat("</a><br/>&#8679; "+item['upvotes']+" &#8681; "+item['downvotes']+"</div><br/>");
         //
-        posts += '<div class="post" onclick="goToPost('+id+');"><a style="font-size:100%;text-decoration:none;" href="post.html?id='+id+'"><b>'+title+'</b></a><div class="meta"><span>Posted by: <a href="user.html?id='+item['poster']+'">'+item['poster']+'</a><br>&#8679; '+item['upvotes']+' &#8681; '+item['downvotes']+'</a></span></div></div>';
+        postsHTML += '<div class="post" onclick="goToPost('+id+');"><a style="font-size:100%;text-decoration:none;" href="post.html?id='+id+'"><b>'+title+'</b></a><div class="meta"><span>Posted by: <a href="user.html?id='+item['poster']+'">'+item['poster']+'</a><br>&#8679; '+item['upvotes']+' &#8681; '+item['downvotes']+'</a></span></div></div>';
         lastid = id;
       
     }catch (err){
@@ -22,14 +22,36 @@ function loadPosts(){
     var xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         var tmp = JSON.parse(xhttp.responseText);
-		$("posts").innerHTML = '';
+        $("posts").innerHTML = '';
+        $("loadmorecontainer").style.display = "";
 		for (var i = tmp['totalposts']; i > tmp['totalposts']-20; i--) {
 			toLink(i,tmp[i]);
         }
-        $("posts").innerHTML = posts;
+        $("posts").innerHTML = postsHTML;
     };
     xhttp.open("GET", "https://api.stibarc.com/v2/getposts.sjs", true);
     xhttp.send();
+}
+
+var lastid = 1;
+var lastfollowid = 1;
+
+function loadMore() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        if (this.responseText.trim() != "") {
+            var tmp = JSON.parse(this.responseText);
+            var tmp2 = lastid-1;
+            for (var i = tmp2; i > tmp2-20; i--) {
+                toLink(i,tmp[i]);
+            }
+            $("posts").innerHTML = postsHTML;
+        } else {
+            $("loadmorecontainer").style.display = "none";
+        }
+    }
+    xhttp.open("GET", "https://api.stibarc.com/v2/getposts.sjs?id="+lastid, true);
+	xhttp.send(null);
 }
 
 loadPosts();
